d913462eb771025c889b76146d10d21836aa27a2
Maykon Emanuel <maykontaio@hotmail.com>
2024-06-01 16:00:00 +0000

    Integration

--- integration/api/v1/status/get.test.js
+++ integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test("GET api/v1/status should return status code 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  const version = responseBody.dependencies.database.version;
+  expect(version).toEqual("16.0");
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  expect(maxConnections).toEqual(100);
+  const openedConnections =
+    responseBody.dependencies.database.opened_connections;
+  expect(openedConnections).toEqual(1);
 });
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
-import database from "../../../../infra/database.js";
+import database from "/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SOMA");
-  console.log(result.rows);
-  response.status(200).json({ frase: "Hello World o/ o/" });
+  const dataAtual = await new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datName = current_database()",
+  );
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: dataAtual,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        opened_connections: databaseOpenedConnectionsValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+      },
+    },
+  });
 }
 
 export default status;
