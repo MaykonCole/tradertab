b6836999569f28c889ea370eafaafb1223ae3745
Maykon Emanuel <maykontaio@hotmail.com>
2024-11-23 19:38:27 +0000

    Endpoint Migrations method not allowed

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,38 +3,48 @@ import { join } from "node:path";
 import database from "../../../../infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+  let dbClient;
+  if (request.method === "GET" || request.method === "POST") {
+    try {
+      dbClient = await database.getNewClient();
 
-  const defaultMigrations = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+      const defaultMigrations = {
+        dbClient: dbClient,
+        dryRun: true,
+        dir: join("infra", "migrations"),
+        direction: "up",
+        verbose: true,
+        migrationsTable: "pgmigrations",
+      };
 
-  if (request.method === "GET") {
-    console.log("Entrou no GET");
-    const pendingMigrations = await migrationRunner(defaultMigrations);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
+      if (request.method === "GET") {
+        console.log("Entrou no GET");
+        const pendingMigrations = await migrationRunner(defaultMigrations);
+        await dbClient.end();
+        return response.status(200).json(pendingMigrations);
+      }
 
-  if (request.method === "POST") {
-    console.log("Entrou no POST");
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrations,
-      dryRun: false,
-    });
+      if (request.method === "POST") {
+        console.log("Entrou no POST");
+        const migratedMigrations = await migrationRunner({
+          ...defaultMigrations,
+          dryRun: false,
+        });
 
-    await dbClient.end();
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+        if (migratedMigrations.length > 0) {
+          return response.status(201).json(migratedMigrations);
+        }
+        return response.status(200).json(migratedMigrations);
+      }
+    } catch (error) {
+      console.log("Endpoint Migrations : " + error);
+      throw error;
+    } finally {
+      await dbClient.end();
     }
-    return response.status(200).json(migratedMigrations);
   }
 
-  return response.status(405);
+  return response.status(405).json({
+    error: `Method not allowed : ${request.method}`,
+  });
 }
