95a0482366950dc2daf240f4552a100ab5a00b84
Maykon Emanuel <maykontaio@hotmail.com>
2024-12-28 23:34:30 +0000

    refactor: adjust tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -8,32 +8,38 @@ describe("POST/api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("For the first", async () => {
-        const response = await fetch(
+        const responseCreated = await fetch(
           "http://localhost:3000/api/v1/migrations",
           {
             method: "POST",
           },
         );
 
-        expect(response.status).toBe(201);
+        expect(responseCreated.status).toBe(201);
 
-        const responseBody = await response.json();
-        expect(Array.isArray(responseBody)).toBe(true);
-        expect(responseBody.length).toBe(1);
+        const responseBodyCreated = await responseCreated.json();
+        expect(Array.isArray(responseBodyCreated)).toBe(true);
+        expect(responseBodyCreated.length).toBe(1);
       });
+    });
+  });
 
-      test("For the second", async () => {
-        const response = await fetch(
-          "http://localhost:3000/api/v1/migrations",
-          {
-            method: "POST",
-          },
-        );
+  describe("POST/api/v1/migrations", () => {
+    describe("Anonymous user", () => {
+      describe("Running pending migrations", () => {
+        test("For the second", async () => {
+          const responseSuccess = await fetch(
+            "http://localhost:3000/api/v1/migrations",
+            {
+              method: "POST",
+            },
+          );
 
-        expect(response.status).toBe(200);
-        const responseBody = await response.json();
-        expect(Array.isArray(responseBody)).toBe(true);
-        expect(responseBody.length).toBe(0);
+          expect(responseSuccess.status).toBe(200);
+          const responseBodySuccess = await responseSuccess.json();
+          expect(Array.isArray(responseBodySuccess)).toBe(true);
+          expect(responseBodySuccess.length).toBe(0);
+        });
       });
     });
   });
