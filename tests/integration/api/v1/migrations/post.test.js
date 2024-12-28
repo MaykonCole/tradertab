4cf4bffd490545fad9a4226c4b1c4ce582b34e5f
Maykon Emanuel <maykontaio@hotmail.com>
2024-12-28 22:00:49 +0000

    refactor: desbrible tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -4,15 +4,37 @@ beforeAll(async () => {
   await basetest.stabilizesEnvironment("migrations");
 });
 
-test("POST api/v1/migrations should return status code 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST/api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(response.status).toBe(201);
+
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(1);
+      });
+
+      test("For the second", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        expect(response.status).toBe(200);
+        const responseBody = await response.json();
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBe(0);
+      });
+    });
   });
-  expect(response.status).toBe(201);
-
-  const responseBody = await response.json();
-
-  expect(Array.isArray(responseBody)).toBe(true);
-
-  expect(responseBody.length).toBeGreaterThan(0);
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,19 +4,21 @@ beforeAll(async () => {
   await basetest.stabilizesEnvironment("status");
 });
 
-test("GET api/v1/status should return status code 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving API status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
+      const version = responseBody.dependencies.database.version;
+      expect(version).toEqual("16.0");
+      const maxConnections = responseBody.dependencies.database.max_connections;
+      expect(maxConnections).toEqual(100);
 
-  console.log(responseBody.dependencies.database.opened_connections);
-  const version = responseBody.dependencies.database.version;
-  expect(version).toEqual("16.0");
-  const maxConnections = responseBody.dependencies.database.max_connections;
-  expect(maxConnections).toEqual(100);
-
-  const openedConnections =
-    responseBody.dependencies.database.opened_connections;
-  expect(openedConnections).toBeLessThan(3);
+      const openedConnections =
+        responseBody.dependencies.database.opened_connections;
+      expect(openedConnections).toBeGreaterThan(1);
+    });
+  });
 });
